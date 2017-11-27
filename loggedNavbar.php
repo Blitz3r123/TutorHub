b0e8dfea12fedcef8f9c5982f30412adf9689df2
Kaleem <KaleemPeeroo@gmail.com>
2017-11-27 12:58:13 +0000

    Second commit

--- loggedNavbar.php
+++ loggedNavbar.php
@@ -4,21 +4,13 @@
 	</button>
 	<ul class="navbar-nav">
 		<li class="nav-item">
+			<a href="dashboard.php" class="navbar-brand">TutorHub</a>
+		</li>
+		<li class="nav-item">
 			<a href="about.php" class="nav-link">About</a>
 		</li>
 		<li class="nav-item">
 			<a href="contact.php" class="nav-link">Contact</a>
-		</li>
-	</ul>
-	<ul class="navbar-nav mr-auto justify-content-center mx-auto">
-		<li class="nav-item"><a href="index.php" class="navbar-brand">TutorHub</a></li>
-	</ul>
-	<ul class="navbar-nav">
-		<li class="nav-item">
-			<a href="login.php" class="nav-link">Login</a>
-		</li>
-		<li class="nav-item">
-			<a href="register.php" class="nav-link">Register</a>
 		</li>
 	</ul>
 </nav>
